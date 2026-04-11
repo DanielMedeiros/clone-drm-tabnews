d6e57f742e9692f978d457e2b83272d8a44f06f0
Daniel R Medeiros <kabal79@gmail.com>
2026-04-11 11:28:31 -0300

    Ajuste para teste em produção

--- infra/database.js
+++ infra/database.js
@@ -8,9 +8,17 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
   });
-  await client.connect();
+
+  console.log("Credenciais :>> ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
